d4f7775c4a633fad388241a9674cfa3ac99d2c95
marcinkrzyzynski <marcinkrzyzynski@gmail.com>
2025-08-07 14:58:48 +0200

    up

--- hello_guys.py
+++ hello_guys.py
@@ -1,4 +1,15 @@
-import os,sys
+from __future__ import print_function
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+# create an instance of the API class
+api_instance = swagger_client.MoviesApiApi()
+api_key = '[add user key]' # str |
 
 
-print('czesc')
+krtry:
+    api_response = api_instance.a_pi_top250_movies_api_key_get(api_key)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling MoviesApiApi->a_pi_top250_movies_api_key_get: %s\n"% e)
